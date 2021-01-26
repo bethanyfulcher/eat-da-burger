52151fec0027f911b9d83160f7663842740f2383
Bethany Fulcher <fulcherbethany@gmail.com>
2021-01-25 20:18:57 -0800

    completed models folder

--- models/burger.js
+++ models/burger.js
@@ -1,3 +1,24 @@
 // Import orm.js
+const orm = require("../config/orm.js");
 // Call the ORM functions using burger specific input for the ORM
+
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res)
+        })
+    },
+    create: function(columns, values, cb) {
+        orm.create("burgers", columns, values, function(res) {
+            cb(res)
+        })
+    },
+    update: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+}
+
 // Export
+module.exports = burger;
