e1fa7f1152e0ba81b67eaf9bd539c38c485552c5
Bethany Fulcher <fulcherbethany@gmail.com>
2021-01-27 09:43:32 -0800

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Setup the code to connect Node to MySQL
-var mysql = require("mysql2");
+const mysql = require("mysql2");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+let connection;
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
+
 
 // Make connection.
 connection.connect(function(err) {
